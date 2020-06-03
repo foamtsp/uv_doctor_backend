ff4bef48f365f71b455be72692d6b5aec4e848c2
foamtsp <foamrock@gmail.com>
2020-06-03 14:12:23 +0700

    change DB

--- mongoUtil.js
+++ mongoUtil.js
@@ -5,15 +5,23 @@ const MongoClient = require('mongodb').MongoClient;
 let doctor_database
 let patient_database 
 
+let USER = ""
+let PWD = ""
+
 connection = (callback) => {
-  const uri = "mongodb+srv://whisper:ultraviolet64@cluster0-fdqs9.gcp.mongodb.net/test?retryWrites=true&w=majority";
+  const uri = "mongodb+srv://"+ USER + ":"+ PWD + "@cluster0-47qvl.gcp.mongodb.net/test?retryWrites=true&w=majority";
   const client = new MongoClient(uri, { keepAlive: 1,useUnifiedTopology: true,useNewUrlParser: true });
   client.connect((err,client) => {
-    console.log("Successful connect to database")
-    // perform actions on the collection object
-    doctor_database = client.db('doctor')
-    patient_database = client.db('patient')
-    return callback(err)
+    if (err){
+      console.log("Database Connection Error")
+    }
+    else{
+      console.log("Successful connect to database")
+      // perform actions on the collection object
+      doctor_database = client.db('doctor')
+      patient_database = client.db('patient')
+    }
+    // return callback(err)
   })     
 }
 
